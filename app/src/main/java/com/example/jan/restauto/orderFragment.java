e4523a2f19ade25955dbda1f27660ad1afc8ff4d
CRemolado <37782441+CRemolado@users.noreply.github.com>
2018-04-27 17:06:43 -0400

    Implemented: -Simulation of sending to kitchen and kitchen notification message -JSON parsing methods to populate lists
    Left:
    -Get tableID and orderID to show on notification message
    -Create DB tables for each order and all orders
    -Populate lists with said DB tables
    -Change orderedItems list when different order is chosen from allOrders list
    -Remove order from allOrders list when SEND is clicked

--- app/src/main/java/com/example/jan/restauto/orderFragment.java
+++ app/src/main/java/com/example/jan/restauto/orderFragment.java
@@ -1,12 +1,17 @@
 package com.example.jan.restauto;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.CountDownTimer;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,13 +20,27 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.os.CountDownTimer;
+import android.app.NotificationChannel;
+import android.support.v7.app.AlertDialog;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.braintreepayments.api.BraintreeFragment;
 import com.braintreepayments.api.dropin.DropInActivity;
 import com.braintreepayments.api.dropin.DropInRequest;
 import com.braintreepayments.api.dropin.DropInResult;
 import com.braintreepayments.api.exceptions.InvalidArgumentException;
 import com.braintreepayments.api.models.PaymentMethodNonce;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 /**
@@ -42,8 +61,29 @@ public class orderFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    public View view;
+
+    public ListView mListView1;
+    public ListView mListView2;
+
+    public ListAdapter mListAdapter1;
+    public ListAdapter mListAdapter2;
+    public ArrayAdapter mArrayAdapter;
+
+    //public String[] orderedFood;
+    //public String[] allOrders;
+
     private Button payBtn;
-    ListView mListView;
+    private Button sendBtn;
+
+    private ProgressBar cookProgress;
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 49527;
+
+    public RequestQueue mQueue;
+
+
     private String clientToken = "sandbox_tgrkgfp7_fjshk9xnbdgzmsvt";
     int REQUEST_CODE;
     BraintreeFragment mBraintreeFragment;
@@ -75,6 +115,12 @@ public class orderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Notification start creation
+        notification = new NotificationCompat.Builder(getContext());
+        notification.setAutoCancel(false);
+
+        //
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -91,16 +137,24 @@ public class orderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_order, container, false);
-        String[] orderedFood={"Item A     price","Item B      price","Item C      price","Item D       price","Item E      price","Item F      price","Item G      price"};
-        ListAdapter mAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,orderedFood);
-        mListView=(ListView) view.findViewById(R.id.orderedFoodList);
-        mListView.setAdapter(mAdapter);
+        view = inflater.inflate(R.layout.fragment_order, container, false);
 
-        String[] allOrders={"Order 1","Order 2","Order 3","Order 4","Order 5","Order 6","Order 7"};
-        ListAdapter mAdapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_activated_1,allOrders);
-        mListView=(ListView) view.findViewById(R.id.allOrdersList);
-        mListView.setAdapter(mAdapter2);
+        //JSON Parsing
+        mQueue = Volley.newRequestQueue(getContext());
+        getMenu();
+
+        //List of food for chosen order
+        //String[] orderedFood={"Item A     price","Item B      price","Item C      price","Item C      price","Item C      price"};
+        //mListAdapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,orderedFood);
+        //mListView1=(ListView) view.findViewById(R.id.orderedFoodList);
+        //mListView1.setAdapter(mAdapter);
+
+        //All orders, with one selected
+        String[] allOrders={"Order 1","Order 2","Order 3"};
+        mListAdapter2 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_activated_1,allOrders);
+        mListView2 = (ListView) view.findViewById(R.id.allOrdersList);
+        mListView2.setAdapter(mListAdapter2);
+
         // Inflate the layout for this fragment
         payBtn = view.findViewById(R.id.payBtn);
         payBtn.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +164,18 @@ public class orderFragment extends Fragment {
                         .clientToken(clientToken);
 
                 startActivityForResult(dropInRequest.getIntent(getContext()), REQUEST_CODE);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        cookProgress = view.findViewById(R.id.cookProgress);
+        sendBtn = view.findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //timer & progress bar
+                cookProgress.setProgress(0);
+                cookProgressSim(15000, 150);
             }
         });
         return view;
@@ -125,6 +191,176 @@ public class orderFragment extends Fragment {
         startActivityForResult(dropInRequest.getIntent(getContext()), REQUEST_CODE);
     }
 */
+
+    public void getMenu() {
+        Log.d("menu", "menu called");
+        String getMenu = "http://10.0.2.2:8080/get_menu";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,getMenu,null,new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray menu = response.getJSONArray("get_menu");
+                    int size = menu.length();
+                    JSONObject item;
+                    String[] orderedItem = new String[size];
+
+                    for (int i = 0; i < menu.length(); i++) {
+                        item = menu.getJSONObject(i);
+
+                        String name = item.getString("name");
+                        int itemId = item.getInt("itemId");
+                        String section = item.getString("section");
+                        double price = item.getDouble("price");
+
+                        orderedItem[i] = name + "\t\t\t" + " $" + price;
+
+                        mListView1=(ListView) view.findViewById(R.id.orderedFoodList);
+                        mListAdapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_checked,orderedItem);
+                        mListView1.setAdapter(mListAdapter1);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {//this is what is being done instead of the json
+                        //if(error.networkResponse != null && error.networkResponse.data != null) {
+                        //VolleyError err = new VolleyError(new String(error.networkResponse.data));
+                        //error = err;
+
+                        String[] tet={"no","json","received"};
+                        mListAdapter1=new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,tet);
+                        mListView1.setAdapter(mListAdapter1);
+                        //}
+                        // error.printStackTrace();
+                        //Log.d("error", error.toString());
+                    }
+                });
+
+        mQueue.add(jsonObjectRequest);
+
+    }
+
+
+    //REFER BACK TO THIS
+    /*public void getMenu(){
+        String url = "http://10.0.2.2:8080/get_menu";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("get_menu");
+
+                            for (int i=0; i < jsonArray.length(); i++) {
+                                JSONObject item = jsonArray.getJSONObject(i);
+
+                                int itemID = item.getInt("itemId");
+                                String name = item.getString("name");
+                                String section = item.getString("section");
+                                double price = item.getDouble("price");
+
+                                orderedFood[i] = name + "\t\t\t" + " $" + price;
+                                //orderedFood[i] = "name";
+                            }
+
+                            mListAdapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,orderedFood);
+                            mListView1=(ListView) view.findViewById(R.id.orderedFoodList);
+                            mListView1.setAdapter(mListAdapter1);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                    //this is what is being done instead of the json
+                    //if(error.networkResponse != null && error.networkResponse.data != null) {
+                    //VolleyError err = new VolleyError(new String(error.networkResponse.data));
+                    //error = err;
+
+                    String[] tet={"no","json","received"};
+                    mArrayAdapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,tet);
+                    mListView1.setAdapter(mArrayAdapter);
+                    //error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }*/
+
+    private void cookProgressSim(int millisInFuture, int countDownInterval){
+        new CountDownTimer(millisInFuture, countDownInterval) {
+            public void onTick(long msUntilFinished) {
+                cookProgress.incrementProgressBy(1);
+            }
+
+            public void onFinish() {
+                //notification heads-up: "order for " + tableID + " is ready for pickup"
+                cookProgress.incrementProgressBy(1);
+                cookingFinishedNotif(view);
+            }
+        }.start();
+    }
+
+    public void cookingFinishedNotif(View view){
+        //Builds notification manager
+        NotificationManager nm = (NotificationManager) getActivity().getSystemService(getActivity().NOTIFICATION_SERVICE);
+
+        //Creates notification channel
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            String CHANNEL_ID = "my_channel_01";
+            String name = "my_channel";
+            String Description = "This is my channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            //mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            nm.createNotificationChannel(mChannel);
+        }
+
+        //Build the notification
+        notification.setSmallIcon(R.drawable.ic_launcher_foreground);
+        notification.setTicker("Kitchen has finished an order");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Restaurant Automation");
+        notification.setContentText("order + orderID + for  + tableID + is ready for pickup");
+        //notification.setPriority(2);
+        notification.setOnlyAlertOnce(true);
+
+        Intent intent1 = new Intent(getContext(), orderFragment.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Issues notification
+        nm.notify(uniqueID, notification.build());
+        showAlertDialogButtonClicked();
+    }
+
+    public void showAlertDialogButtonClicked() {
+
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Kitchen finished an order");
+        builder.setMessage("order for  + tableID +  is ready for pickup");
+
+        // add a button
+        builder.setPositiveButton("OK", null);
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
